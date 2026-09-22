f7559d93c4dd2f8ffefbd9d7491f9512c5ec6957
gastonspinelli3-create <gastonspinelli3@gmail.com>
2026-09-22 08:21:35 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,6 @@
+
+const CACHE='jarvis-v5-locutor-grave-v1';
+const ASSETS=['/','/index.html','/manifest.json'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('/index.html'))))});
